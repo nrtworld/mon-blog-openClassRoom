5fc5b473314d8413526bd044121d2b371be0d709
nrtworld <41008836+nrtworld@users.noreply.github.com>
2018-07-10 16:42:24 +0200

    sauvegarde du jour 2

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to mon-blog!');
+    expect(page.getParagraphText()).toEqual('Welcome to mon-projet-angular!');
   });
 });
